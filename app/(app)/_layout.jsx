29700f38a7c96f649d2c750b2f700853a4c37203
koslinj <koslinj@gmail.com>
2024-10-30 18:45:18 +0100

    add header for workoutdetails page

--- app/(app)/_layout.jsx
+++ app/(app)/_layout.jsx
@@ -121,6 +121,18 @@ const AppLayout = () => {
           };
         }}
       />
+      <Stack.Screen name='(screens)/workout/[id]' options={{
+        header: () => (
+          <ThemedView lightClassName='bg-slate-300' darkClassName='bg-slate-800' style={[{ padding: 12 }, styles.safeArea]}>
+            <View>
+              <TouchableOpacity className="absolute z-10" onPress={() => { router.back() }}>
+                <AntDesign color={theme === "light" ? "black" : "white"} name='arrowleft' size={42} />
+              </TouchableOpacity>
+              <ThemedText className='text-3xl text-center font-poppinsBold translate-y-1'>Workout</ThemedText>
+            </View>
+          </ThemedView>
+        )
+      }} />
     </Stack>
   )
 }
